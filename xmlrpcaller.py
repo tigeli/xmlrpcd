aaef89aa890f5a55d8c7def786ab4341fd144477
Stanislaw Klekot <dozzie@jarowit.net>
2010-09-06 22:00:59 +0200

    Added Python XML-RPC caller example.
    This works for a) HTTPs and b) HTTP authentication.

---
+++ xmlrpcaller.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+# server - RPC server address
+# port   - RPC server port
+# creds  - tuple of (username, password); give None if no HTTP-AUTH desired
+# method - method name (most probably: package.function)
+# args   - method arguments
+def xml_rpcall(server, port, creds, method, *args):
+  import xmlrpclib as xmlrpc
+  import httplib as http
+  import base64
+
+  headers = { "Content-Type": "text/xml" }
+  if creds != None:
+    headers["Authorization"] = "Basic %s" % (base64.b64encode('%s:%s' % creds),)
+  body = xmlrpc.dumps(args, methodname = method)
+
+  server = http.HTTPSConnection('localhost', 1638)
+  server.request("POST", "/", body, headers)
+  resp = server.getresponse()
+  if resp.status >= 400:
+    raise xmlrpc.Fault("HTTP error %d (%s)" % (resp.status, resp.reason))
+  xml_resp = resp.read()
+  return xmlrpc.loads(xml_resp)[0][0]
+
+# 'authenticated_example.reverse'
+# 'public_example.sumAndDifference'
+
+print xml_rpcall(
+  'localhost', 1638, ('nabla', 'nabla'),
+  'authenticated_example.reverse', 8, 20
+)
+
+# vim:ft=python
